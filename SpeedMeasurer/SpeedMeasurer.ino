dfb061241228085cc1ebe2ad0d67b04b3759b23d
J-Xiao <xiaojijundavid@gmail.com>
2016-07-11 16:50:27 -0400

    prepare to display to speed

--- SpeedMeasurer/SpeedMeasurer.ino
+++ SpeedMeasurer/SpeedMeasurer.ino
@@ -121,5 +121,7 @@ void loop() {
   }
   average_speed_m_s = (speed_one + speed_two)/2;     // Calculate the average speed to reduce error
   average_speed_km_h = average_speed_m_s*3.6;
-  
+  while(average_speed_km_h){
+
+  }
 }
